f692d3625a650be930259ae3a94842847d80a67f
fadhdwisamns <fadhdwisam@gmail.com>
2021-11-16 15:34:21 +0700

    point 3

--- app/Http/Livewire/Post/Index.php
+++ app/Http/Livewire/Post/Index.php
@@ -5,11 +5,13 @@ namespace App\Http\Livewire\Post;
 use App\Post;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Http\Request;
 
 class Index extends Component
 {
     public $title;
     public $content;
+    public $postId;
 
     public function render()
     {
@@ -43,5 +45,32 @@ class Index extends Component
         return redirect()->route('post.index');
 
     }
+    //  public function mount($id) {
+    //     $post = Post::find($id);
 
+    //     if($post){
+    //         $this->postId = $post->id;
+    //         $this->title = $post->title;
+    //         $this->content = $post->content;
+    //     }
+    // }
+    public function update(){
+        $this->validate([
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        if($this->postId) {
+        $post = Post::find($this->postId);
+        if($post){
+            $post->update([
+                'title' => $this->title,
+                'content' => $this->content
+                ]);
+            }
+        }
+        session()->flash('message' , 'Data Berhasil Diperbarui');
+
+        return redirect()->route('post.index');
+    }
 }
--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <a href="{{ route('post.create')}}" class="btn btn-md btn-success mb-3">TAMBAH POST</a>
+   
     <table class="table table-bordered">
     <thead class = "thead-dark">
         <tr>
@@ -21,13 +21,20 @@
                     </td>
                 </tr>
         @foreach($posts as $post)
-        <tr>
-            <td>{{$post->title}}       
+        <tr wire:ignore>
+            <td>
+            {{$post->title}}
+            <!-- <input type="text" wire:model="title" class="form-control @error('title') is-invalid @enderror"  value="">
+                @error('title')
+                    <span class="invalid-feedback">
+                        {{$message}}
+                    </span>
+                    @enderror -->
             </td>
             <td>{{$post->content}}</td>
             <td class="text-center">
-                <a href="{{ route('post.edit', $post->id)}}"class="btn btn-sm btn-primary">EDIT</a>
-                <button wire:click="destroy({{ $post->id }})" class="btn btn-sm btn-danger">DELETE</button>
+                <a href="{{ route('post.edit', $post->id)}}"class="btn btn-sm btn-success">Save</a>
+                <button wire:click="destroy({{ $post->id }})" class="btn btn-sm btn-danger">Delete</button>
             </td>
         </tr>
         @endforeach
